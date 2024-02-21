8f484b8b682410574d30329dea9a99c2fddb9da9
Andrew Noise <128706037+anoise1@users.noreply.github.com>
2024-02-21 16:51:05 +0300

    Update dispersion.js

--- dispersion.js
+++ dispersion.js
@@ -1,8 +1,1 @@
-let rootStyle = window.getComputedStyle(document.documentElement)
-let thColor = rootStyle.getPropertyValue("--th-theme-bg-color");
-if (thColor.charAt(0) == "#") {
-    thColor = thColor.slice(1);
-}
-let r = parseInt(thColor.slice(0, 2), 16);
-let g = parseInt(thColor.slice(2, 4), 16);
-let b = parseInt(thColor.slice(4, 6), 16);
+
